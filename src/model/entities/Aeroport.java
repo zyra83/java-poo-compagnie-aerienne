207347647f4d624c7aca809f8304655c8230dd5b
zyra83 <zyra83@hotmail.com>
2017-04-12 18:40:58 +0200

    chmod

--- src/model/entities/Aeroport.java
+++ src/model/entities/Aeroport.java
@@ -3,9 +3,8 @@ package model.entities;
 import java.util.List;
 
 /**
- * Objet représentant un Aéroport.
- * Les aéroport sont désservis par des vols ou des escales.
- * Ils désservent eux mêmes des villes.
+ * Objet représentant un Aéroport. Les aéroport sont désservis par des vols ou
+ * des escales. Ils désservent eux mêmes des villes.
  * 
  * @author Mickaël Benoit
  *
@@ -16,11 +15,16 @@ public class Aeroport {
 	 * Le nom commun de l'aéroport.
 	 */
 	private String nom;
-	
+
 	/**
 	 * Liste des villes que déssert l'aéroport.
 	 */
 	private List<Ville> villesDesservies;
+
+	public Aeroport(String nom) {
+		super();
+		this.nom = nom;
+	}
 
 	/**
 	 * @return the nom
@@ -29,31 +33,52 @@ public class Aeroport {
 		return nom;
 	}
 
-	public Aeroport(String nom) {
-		super();
-		this.nom = nom;
-	}
-
-	/**
-	 * @param nom the nom to set
-	 */
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	/**
-	 * @return the villesDesservies
-	 */
 	public List<Ville> getVillesDesservies() {
 		return villesDesservies;
 	}
 
-	/**
-	 * @param villesDesservies the villesDesservies to set
-	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
 	public void setVillesDesservies(List<Ville> villesDesservies) {
 		this.villesDesservies = villesDesservies;
 	}
+
+	// Equals / HashCode sur nom et vilesl
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aeroport other = (Aeroport) obj;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (villesDesservies == null) {
+			if (other.villesDesservies != null)
+				return false;
+		} else if (!villesDesservies.equals(other.villesDesservies))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + ((villesDesservies == null) ? 0 : villesDesservies.hashCode());
+		return result;
+	}
+
+	// toString
 	
 	@Override
 	public String toString() {
